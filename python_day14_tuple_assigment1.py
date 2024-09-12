0a1f724997c84b2e7d1b04efdbac194cb0e99653
Sahil_Deshmukh <124992028+Sahil22-collab@users.noreply.github.com>
2024-09-12 12:05:28 +0530

    Add files via upload

---
+++ python_day14_tuple_assigment1.py
@@ -0,0 +1,14 @@
+#Write a Python program to find the number of times 4 appears in the tuple.
+# Define the tuple
+tuple=(2, 4, 5, 6, 2, 3, 4, 4, 7 )
+
+# Use the count() method to find the number of times 4 appears
+ans=tuple.count(4)
+
+# Print the result
+print(ans)
+
+
+'''Output
+    3'''
+
---
+++ python_day14_tuple_assigment2.py
@@ -0,0 +1,13 @@
+#Write a Python program to convert a list to a tuple.
+
+# Define the list
+listx= [5, 10, 7, 4, 15, 3]
+
+# Convert the list to a tuple
+result=tuple(listx)
+
+# Print the result
+print(result)
+
+'''output
+(5, 10, 7, 4, 15, 3)'''
