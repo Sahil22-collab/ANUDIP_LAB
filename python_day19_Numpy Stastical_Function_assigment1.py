bcd4b90b78db0d5abe029735b95304a41e822b79
Sahil_Deshmukh <124992028+Sahil22-collab@users.noreply.github.com>
2024-09-20 13:57:17 +0530

    Add files via upload

---
+++ python_day19_Numpy Stastical_Function_assigment1.py
@@ -0,0 +1,16 @@
+#Compute the median of the flattened NumPy array
+
+import numpy as np  # Import the NumPy library
+
+# Create a NumPy array with odd numbers
+x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+
+# Compute the median of the array
+median_value = np.median(x_odd)
+
+# Print the median value
+print(median_value)  # Output will be 4.0
+
+
+'''output
+4.0'''
---
+++ python_day19_Numpy Stastical_Function_assigment2.py
@@ -0,0 +1,18 @@
+#Compute the standard deviation of the NumPy array
+
+import numpy as np  # Import the NumPy library
+
+# Create a NumPy array with the given values
+arr = np.array([20, 2, 7, 1, 34])
+
+# Compute the standard deviation of the array
+std_deviation = np.std(arr)
+
+# Print the standard deviation value
+print(std_deviation)  # Output will be the standard deviation
+
+'''output
+12.576167937809991
+'''
+
+
